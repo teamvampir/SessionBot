fef573f698060b1d153cbae5154ad6be551772c1
MUKESH BABU S <108213299+mukeshmoni@users.noreply.github.com>
2023-09-27 11:33:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 API_ID = int(getenv("API_ID", "13382500"))
 API_HASH = getenv("API_HASH", "6f5691b23c6f032eaae722e96d7e2459")
 
-BOT_TOKEN = getenv("BOT_TOKEN", "5873474775:AAGUOTRKKkAIgYzlAG9WU12ei-mgOhnsj3A")
+BOT_TOKEN = getenv("BOT_TOKEN", "5873474775:AAG982rUOp_Dl1R0oweDUH_0GccAuo_uZTo")
 OWNER_ID = int(getenv("OWNER_ID", "6102257906"))
 
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://monivps3:monivps3@cluster0.jnjnbnx.mongodb.net/?retryWrites=true&w=majority")
