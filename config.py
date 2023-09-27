65fc5f972d06e8cc7dc925ca6d21afe6dffea2b5
MUKESH BABU S <108213299+mukeshmoni@users.noreply.github.com>
2023-09-27 11:00:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ load_dotenv()
 API_ID = int(getenv("API_ID", "13382500"))
 API_HASH = getenv("API_HASH", "6f5691b23c6f032eaae722e96d7e2459")
 
-BOT_TOKEN = getenv("BOT_TOKEN", "5873474775:AAF87-0BXKcp0VcIpg0hKm-0QF0zWIqdlwE")
+BOT_TOKEN = getenv("BOT_TOKEN", "5873474775:AAGc4lfENzKCV7Gy4IRfDdSyw2cNXhD2FhA")
 OWNER_ID = int(getenv("OWNER_ID", "6102257906"))
 
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://vps45:vps45@cluster0.97aeulz.mongodb.net/?retryWrites=true&w=majority")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://monivps3:monivps3@cluster0.jnjnbnx.mongodb.net/?retryWrites=true&w=majority")
 MUST_JOIN = getenv("MUST_JOIN", "https://t.me/TMK_MUSICCHANNEL")
