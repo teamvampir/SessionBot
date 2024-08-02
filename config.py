ea6b24bad2c836c768c7f29b5c7217c2b2c5a075
Moni Sandy <148899466+MoniSandy0204@users.noreply.github.com>
2024-08-02 21:26:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 API_ID = int(getenv("API_ID", "13382500"))
 API_HASH = getenv("API_HASH", "6f5691b23c6f032eaae722e96d7e2459")
 
-BOT_TOKEN = getenv("BOT_TOKEN", "5873474775:AAEQRSgniiv3OLzy2Eexagto7z1Mr1nt6_o")
+BOT_TOKEN = getenv("BOT_TOKEN", "5873474775:AAFFG1d-TrJJdrYWvnVPMjnCNCy3CdSpC8A")
 OWNER_ID = int(getenv("OWNER_ID", "6102257906"))
 
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://bommi:bommi@cluster0.aobxtp5.mongodb.net/?retryWrites=true&w=majority")
