cbcccada5c2233dc3ecfae7dc3578b928e5d0bd8
Moni Sandy <148899466+MoniSandy0204@users.noreply.github.com>
2024-08-02 21:51:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID", "13382500"))
-API_HASH = getenv("API_HASH", "6f5691b23c6f032eaae722e96d7e2459")
+API_ID = int(getenv("API_ID", "19197570"))
+API_HASH = getenv("API_HASH", "0401e837796d193ec9ae6e17cb8cfbf8")
 
 BOT_TOKEN = getenv("BOT_TOKEN", "5873474775:AAFFG1d-TrJJdrYWvnVPMjnCNCy3CdSpC8A")
-OWNER_ID = int(getenv("OWNER_ID", "6102257906"))
+OWNER_ID = int(getenv("OWNER_ID", "6798074818"))
 
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://bommi:bommi@cluster0.aobxtp5.mongodb.net/?retryWrites=true&w=majority")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://monivps5:monivps5@cluster0.kmbq8we.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 MUST_JOIN = getenv("MUST_JOIN", "TMK_MUSICCHANNEL")
